c650edd488392bcaf9d000d7c1990287a4f4aff6
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-05-03 15:24:23 +0300

    authentication with react

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
     <div>
       <nav className="navbar navbar-expand navbar-light fixed-top">
         <div className="container">
-          <Link className="navbar-brand" to={"/"}>Home</Link>
-          <div className="collapse navbar-collapse me-5">
+          <Link className="navbar-brand ms-auto" to={"/"}>Home</Link>
+          <div className="collapse navbar-collapse">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <Link className="nav-link" to={"/login"}>Login</Link>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import axios from 'axios';
 
 const Signup = () => {
-  return (
+    
+   const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            firstname: this.firstname,
+            lastname: this.lastname,
+            email: this.firstname,
+            password: this.password,
+            confirmpassword: this.confirmpassword
+        }
+    }
+
+    return (
+        
     <div>
-      <h1>Signup</h1>
+
+      <form onSubmit={handleSubmit}>
+        <h3>Sign Up</h3>
+        <div className="form-group">
+            <label htmlFor="">First Name</label>
+            <input type="text" className='form-control' placeholder='First Name' onChange={e => this.firstname = e.target.value} />
+        </div>
+        <div className="form-group">
+            <label htmlFor="">Last Name</label>
+            <input type="text" className='form-control' placeholder='Last Name' onChange={e => this.lastname = e.target.value} />
+        </div>
+        <div className="form-group">
+            <label htmlFor="">Email</label>
+            <input type="email" className='form-control' placeholder='Email' onChange={e => this.email = e.target.value} />
+        </div>
+        <div className="form-group">
+            <label htmlFor="">Password</label>
+            <input type="password" className='form-control' placeholder='Password' onChange={e => this.password = e.target.value} />
+        </div>
+        <div className="form-group">
+            <label htmlFor="">Confirm Password</label>
+            <input type="password" className='form-control' placeholder='Confirm Password' onChange={e => this.confirmpassword = e.target.value} />
+        </div>
+        <button type='submit' className='btn btn-primary btn-block mt-3'>Sign Up</button>
+      </form>
     </div>
   )
 }
